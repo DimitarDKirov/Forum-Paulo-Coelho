18b3be120dc8ae5f121bd491e1b3bb02ea74ad53
toshko <toshkooooooooooo@gmail.com>
2015-11-13 09:22:48 +0200

    initial commit Add skeleton

---
+++ Forum-Paulo-Coelho/ForumSystem.Api/Controllers/ThreadsController.cs
@@ -0,0 +1,30 @@
+﻿namespace ForumSystem.Api.Controllers
+{
+    using System.Linq;
+    using System.Web.Http;
+    using ForumSystem.Data;
+    using ForumSystem.Models;
+
+    public class ThreadsController : ApiController
+    {
+        private IRepository<Thread> threads;
+
+        public ThreadsController()
+            :this(new EfGenericRepository<Thread>(new ForumDbContext()))
+        {
+        }
+
+        public ThreadsController(IRepository<Thread> threads)
+        {
+            this.threads = threads;
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetAll()
+        {
+            var threads = this.threads.All().ToList();
+
+            return Ok(threads);
+        }
+    }
+}
