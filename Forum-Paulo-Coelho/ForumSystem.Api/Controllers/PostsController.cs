8ebb254d904360d900e9618e7de4ee1d1b61ec78
DimitarDKirov <dimkirov@gmail.com>
2015-11-14 15:31:32 +0200

    Added posts controller with search by post ID and thread ID

---
+++ Forum-Paulo-Coelho/ForumSystem.Api/Controllers/PostsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using ForumSystem.Data;
+using ForumSystem.Models;
+
+namespace ForumSystem.Api.Controllers
+{
+    public class PostsController:ApiController
+    {
+        private IRepository<Post> postsRepo;
+
+        public PostsController(IRepository<Post> posts)
+        {
+            this.postsRepo = posts;
+        }
+
+        public IHttpActionResult Get(int postId)
+        {
+            var post = this.postsRepo
+                .GetById(postId);
+
+            return this.Ok(post);
+        }
+
+        public IHttpActionResult GetByThread(int threadId)
+        {
+            var posts = this.postsRepo
+                .All()
+                .Where(p => p.ThreadId == threadId)
+                .ToList();
+
+            return this.Ok(posts);
+        }
+    }
+}
