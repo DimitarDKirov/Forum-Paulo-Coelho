fd5cf92826ad873fcded920beb979a155a175c99
DimitarDKirov <dimkirov@gmail.com>
2015-11-17 02:35:03 +0200

    Added factories for test objects

--- Forum-Paulo-Coelho/Tests/ForumSystem.Services.Test/TestObjects/TestObjectsFactory.cs
+++ Forum-Paulo-Coelho/Tests/ForumSystem.Services.Test/TestObjects/TestObjectsFactory.cs
@@ -9,7 +9,7 @@ namespace ForumSystem.Services.Test.TestObjects
 {
     public static class TestObjectsFactory
     {
-        public static MemoryRepository<User> GetUserRepository()
+        public static MemoryRepository<User> GetUsersRepository()
         {
             var usersRepository = new MemoryRepository<User>();
             for (int i = 0; i < 10; i++)
@@ -17,14 +17,91 @@ namespace ForumSystem.Services.Test.TestObjects
                 var user = new User
                 {
                     Nickname = "Nick " + i,
-                    Id="id"+i,
-                    UserName="User"+i
+                    Id = "id" + i,
+                    UserName = "User" + i
                 };
 
                 usersRepository.Add(user);
             }
 
             return usersRepository;
+        }
+
+        public static MemoryRepository<Post> GetPostsRepository()
+        {
+            var postsRepository = new MemoryRepository<Post>();
+            for (int i = 0; i < 20; i++)
+            {
+                var post = new Post
+                {
+                    Content = "content" + i,
+                    Id = i,
+                    PostDate = new DateTime(2015, 11, i),
+                    ThreadId = i % 5,
+                    UserId = "id" + i % 10
+                };
+
+                postsRepository.Add(post);
+            }
+
+            return postsRepository;
+        }
+
+        public static MemoryRepository<Thread> GetThreadsRepository()
+        {
+            var threadsRepository = new MemoryRepository<Thread>();
+            for (int i = 0; i < 5; i++)
+            {
+                var thread = new Thread
+                {
+                    Id = i,
+                    Content = "content" + i,
+                    DateCreated = new DateTime(2015, 11, i),
+                    Title = "title" + i,
+                    UserId = "id" + i % 10,
+                };
+
+                threadsRepository.Add(thread);
+            }
+
+            return threadsRepository;
+        }
+
+        public static MemoryRepository<Comment> GetCommentsRepository()
+        {
+            var commentsRepository = new MemoryRepository<Comment>();
+            for (int i = 0; i < 20; i++)
+            {
+                var comment = new Comment
+                {
+                    Id = i,
+                    CommentDate = new DateTime(2015, 11, i),
+                    Content = "content" + i,
+                    PostId = i,
+                    UserId = "id" + i % 10,
+                };
+
+                commentsRepository.Add(comment);
+            }
+
+            return commentsRepository;
+        }
+
+        public static MemoryRepository<Category> GetCategoriesRepository()
+        {
+            var categoriesRepository = new MemoryRepository<Category>();
+            for (int i = 0; i < 10; i++)
+            {
+                var category = new Category
+                {
+                    Id = i,
+                    Name = "category" + i
+                };
+
+                categoriesRepository.Add(category);
+            }
+
+            return categoriesRepository;
         }
     }
 }
