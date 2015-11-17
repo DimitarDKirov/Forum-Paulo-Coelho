9674e64f6a048c2b6c1c4daf78eee236d3c62bbb
toshko <toshkooooooooooo@gmail.com>
2015-11-17 19:08:10 +0200

    add ThreadsServiceTests.cs

---
+++ Forum-Paulo-Coelho/Tests/ForumSystem.Services.Test/ThreadsServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ForumSystem.Data;
+using ForumSystem.Models;
+using ForumSystem.Services.Contracts;
+using ForumSystem.Services.Test.TestObjects;
+
+namespace ForumSystem.Services.Test
+{
+    [TestClass]
+    public class ThreadsServiceTests
+    {
+        private IRepository<Thread> threads;
+        private IRepository<User> users;
+        private IThreadService service;
+
+        [TestInitialize]
+        public void Init()
+        {
+            this.users = TestObjectsFactory.GetUserRepository();
+           // this.service = new ThreadService()
+        }
+    }
+}
