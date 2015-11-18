416f4fbe3e7102e459b2c7375814b585a074522e
RosenTodorov <kobe_8@abv.bg>
2015-11-18 18:26:25 +0200

    Start to Create CommentsControllerTests

---
+++ Forum-Paulo-Coelho/Tests/ForumSystem.Api.Tests/ControllersTests/CommentsControllerTest.cs
@@ -0,0 +1,19 @@
+﻿namespace ForumSystem.Api.Tests.ControllersTests
+{
+    using System.Collections.Generic;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using ForumSystem.Services.Contracts;
+    using ForumSystem.Api.Tests.TestObjects;
+    using ForumSystem.Api.Controllers;
+    using System.Web.Http.Results;
+    using ForumSystem.Api.Models;
+
+    public class CommentsControllerTest
+    {
+        [TestInitialize]
+        public void Init()
+        {
+            this.postsService = TestObjectsFactory.GetPostsService();
+        }
+    }
+}
